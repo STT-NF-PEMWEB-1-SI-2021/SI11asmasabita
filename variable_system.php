e6bb6f74dc5253cfc2d548ddfcaf5b8e9f58a967
asmasabita <92282146+asmasabita@users.noreply.github.com>
2022-02-26 15:53:34 +0700

    Add files via upload

---
+++ variable_system.php
@@ -0,0 +1,4 @@
+<?php
+echo 'Dokumen Root ' .$_SERVER["DOCUMENT_ROOT"];
+echo '<br/> Nama file ' .$_SERVER["PHP_SELF"];
+?>
---
+++ variable_user.php
@@ -0,0 +1,13 @@
+<?php
+//definisikan variable
+$nama = 'Asma Sabita';
+$umur = 20;
+$berat = 40;
+
+echo 'nama : ' .$nama;
+echo 'Umur : ' .$umur. 'Kg';
+
+echo "<br/> Hello $nama Apa kabar";
+echo "<br/> Hai Nama saya $nama Umur saya $umur berat badan saya $berat kg, salam kenal semuanya";
+echo "<hr>";
+?>
